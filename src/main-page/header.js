785d0795b0f4dd9607209c8650735247ad9396e6
Brian <wamwangimathenge@gmail.com>
2023-08-04 13:00:41 +0300

    Pass in the props object into the Header function
    This commit passes into the function the props object as an argument
    This is used to render the value of the subtitle property of the
    props object

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,12 +1,12 @@
 import logo from "./GloboLogo.png";
 
-const Header = () => (
+const Header = (props) => (
     <header className="row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="logo" />
         </div>
         <div className="col-md-7 mt-5 subtitle">
-            Providing houses all over the world
+            {props.subtitle}
         </div>
     </header>
 )
