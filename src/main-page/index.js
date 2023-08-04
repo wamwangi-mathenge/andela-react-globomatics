a8649c3f9c9f71f2f4ffaac7001212cee63c771f
Brian <wamwangimathenge@gmail.com>
2023-08-04 13:20:08 +0300

    Fetch the houses
    The call to fetch returns a response object which gives the json
    object which is an array of houses

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,8 +1,15 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './main-page.css';
 import Header from "./header"
 
 function App() {
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const rsp = await fetch("./houses.json");
+      const houses = await rsp.json();
+    };
+    fetchHouses();
+  }, [])
   return (
     <div className="container">
       <Header subtitle="Providing houses all over the world"/>
