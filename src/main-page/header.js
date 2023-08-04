2276ca638d1b7c20f5661272cad92b0f2773ec98
Brian <wamwangimathenge@gmail.com>
2023-08-04 13:06:02 +0300

    Refactor the code by destructuring
    This commit gets the subtitle property from the props object by
    putting it between curly braces. Now just the subtitle string
    property is available in the rest of the function, not the whole
    props object

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -1,12 +1,12 @@
 import logo from "./GloboLogo.png";
 
-const Header = (props) => (
+const Header = ({ subtitle }) => (
     <header className="row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="logo" />
         </div>
         <div className="col-md-7 mt-5 subtitle">
-            {props.subtitle}
+            {subtitle}
         </div>
     </header>
 )
